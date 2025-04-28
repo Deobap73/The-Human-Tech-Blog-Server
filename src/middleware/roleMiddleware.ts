22d1b050b1be9872ea37b6d44ec488717b7973cf
Deobap73 <contact@deolindobaptista.com>
2025-04-28 16:44:07 +0200

    small fix in roleMiddleware.ts
     Changes to be committed:
    	new file:   .dockerignore
    	modified:   .gitignore
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/middleware/roleMiddleware.ts

--- src/middleware/roleMiddleware.ts
+++ src/middleware/roleMiddleware.ts
@@ -1,11 +1,12 @@
 // The-Human-Tech-Blog-Server/src/middleware/roleMiddleware.ts
 
 import { Request, Response, NextFunction } from 'express';
+import { IUser } from '../models/User';
 
 export const authorize =
   (...roles: string[]) =>
   (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user || !roles.includes(req.user.role)) {
+    if (!req.user || !roles.includes((req.user as IUser).role)) {
       return res.status(403).json({ message: 'Access denied: insufficient role' });
     }
     next();
